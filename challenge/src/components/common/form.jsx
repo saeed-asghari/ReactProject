5dae2398f5359c8a0b65ce0b09d9b078081c49ec
Saeed Asghari <saeed.asghari.241@gmail.com>
2021-07-17 18:40:36 +0430

    add validation joi setting to component form

--- challenge/src/components/common/form.jsx
+++ challenge/src/components/common/form.jsx
@@ -5,7 +5,22 @@ class Form extends Component {
     data: {},
     errors: {},
   };
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.data, this.schema, options);
+    if (!error) return null;
 
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+
+    const { error } = Joi.validate(obj, schema, { abortEarly: false }); // age ye schema bashe ba chanta error akharin error ro namyesh mide
+    return error ? error.details[0].message : null;
+  };
 }
 
 export default Form;
