278e1fe14b80671f10fa9b694c259443896b72fa
Saeed Asghari <saeed.asghari.241@gmail.com>
2021-07-17 18:52:32 +0430

    add raw login form

---
+++ challenge/src/components/login.jsx
@@ -0,0 +1,11 @@
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return <div>Login Form</div>
+    }
+}
+ 
+export default Login;
