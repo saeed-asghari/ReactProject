6200d35d8e527b9a1e3298a9d965ce3dc011c939
Saeed Asghari <saeed.asghari.241@gmail.com>
2021-07-18 21:46:22 +0430

    login and logout is completed

--- challenge/src/components/main.jsx
+++ challenge/src/components/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./login";
+import Logout from "./logout";
 const Main = () => {
   return (
     <React.Fragment>
       <main className="container col-9 offset-3">
         <Switch>
           <Route path="/login" component={Login} />
+          <Route path="/logout" component={Logout} />
         </Switch>
       </main>
     </React.Fragment>
--- challenge/src/components/navBar.jsx
+++ challenge/src/components/navBar.jsx
@@ -5,30 +5,37 @@ const NavBar = ({ user }) => {
   return (
     <React.Fragment>
       <nav className="navbar navbar-expand  flex-column flex-md-row bd-navbar Background-color-nav ">
-        <span className="navbar-brand">
-          <NavLink to="/" className="text-white"> Challenge </NavLink>
-        </span>
-        <span className="text-white">
-          WelCome {user.username}
-        </span>
-        <div id="navb" className="navbar-collapse collapse hide">
-          {!user && (
-            <ul className="nav navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink to="/login"> Login </NavLink> /
-              </li>
-              <li className="nav-item">
-                <NavLink to="/register"> Register </NavLink>
-              </li>
-            </ul>
-          )}
-          {user && (
-            <button className="btn btn-outline-info">
-              <NavLink to="/logout">Logout</NavLink>
-            </button>
-          )}
-        </div>
-            </nav>
+          <div className="navbar-header">
+            <span className="navbar-brand">
+              <NavLink to="/" className="text-white">
+                Challenge
+              </NavLink>
+            </span>
+            <span className="text-white">Welcome {user && user.username}</span>
+          </div>
+          <div id="navb" className="collapse navbar-collapse ">
+            {!user && (
+              <ul className="nav navbar-nav ml-auto">
+                <li className="nav-item">
+                  <NavLink to="/login"> Login </NavLink> /
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/register"> Register </NavLink>
+                </li>
+              </ul>
+            )}
+            {user && (
+              <ul class="nav navbar-nav ml-auto">
+                <li>
+                  <button className="btn btn-outline-info">
+                    <NavLink to="/logout">Logout</NavLink>
+                  </button>
+                </li>
+              </ul>
+            )}
+          </div>
+        
+      </nav>
     </React.Fragment>
   );
 };
